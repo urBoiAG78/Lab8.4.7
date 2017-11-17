0f7b7c83054659bcf94e597d9a4d07739b3f9763
Anthony Gonzalez <anthonyg9942@gmail.com>
2017-11-17 13:58:12 -0500

    le
    mayo

--- main.cpp
+++ main.cpp
@@ -16,19 +16,14 @@
 
 using namespace std;
 
-/*
- * 
-
- */
 class Matrix {
 public:
-    Matrix(int a, int b, int c, int d){
-    
-    v[0][0]=a;
-    v[0][1]=b;
-    v[1][0]=c;
-    v[1][1]=d;
-}
+    Matrix(int n1, int n2, int n3, int n4){
+        v[0][0]=n1;
+        v[0][1]=n2;
+        v[1][0]=n3;
+        v[1][1]=n4;
+    }
     void print(ostream &os) const{
         cout << v[0][0]<<" "<< v[0][1]<< endl;
         cout << v[1][0]<< " "<< v[1][1]<< endl;
@@ -43,7 +38,11 @@ public:
             }
         }
     }
-    void add(Matrix& m){
+    void operator+(const int& rhs){
+        add(rhs);
+    }
+    
+    void add( Matrix& m){
         for(int i =0; i < 2; i++)
         {
             for(int j =0; j < 2; j++)
@@ -51,6 +50,9 @@ public:
                 v[i][j] += m.v[i][j];
             }
         }
+    }
+    void operator+(Matrix& n){
+        add(n);
     }
     void subtract(const int& s)
     {
@@ -71,28 +73,43 @@ public:
             }
         }
     }
-    void operator+(const int& rhs){
-        add(rhs);
+    void operator-(const int& rhs)
+    {
+        subtract(rhs);
     }
-    void operator+(const Matrix& m){
-        add(m);
+    void operator-(Matrix& m){
+        subtract(m);
     }
-    
 private:
+    
     int v[2][2];
 };
 
+
+
+
+
 int main(int argc, char** argv) {
+
     Matrix m(3,4,5,6);
     Matrix n(3,7,2,5);
+    
     m.print(cout);
     n.print(cout);
     
     
     
     m.add(n);
-    
     m.print(cout);
+    m - n;
+    m.print(cout);
+    
+    m +2;
+    m.print(cout);
+    m-2;
+    m.print(cout);
+    
+    
     return 0;
 }
 
