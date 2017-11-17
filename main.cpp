7e3d425cc4d73f8277729036fd2fdc5ddb2394ab
Anthony Gonzalez <anthonyg9942@gmail.com>
2017-11-17 13:09:53 -0500

    labz

--- main.cpp
+++ main.cpp
@@ -12,12 +12,57 @@
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 /*
  * 
+
  */
+class Matrix {
+public:
+    Matrix(int a, int b, int c, int d){
+    
+    v[0][0]=a;
+    v[0][1]=b;
+    v[1][0]=c;
+    v[1][1]=d;
+}
+    void print(ostream &os) const{
+        cout << v[0][0]<<" "<< v[0][1]<< endl;
+        cout << v[1][0]<< " "<< v[1][1]<< endl;
+        cout<< "--------"<< endl;
+    }
+    void add(const int& rhs){
+        for(int i =0; i < 2; i++)
+        {
+            for(int j =0; j < 2; j++)
+            {
+                v[i][j] += rhs;
+            }
+        }
+    }
+    void operator+(const int& rhs){
+        add(rhs);
+    }
+    
+    void add(Matrix& m){
+        for(int i =0; i < 2; i++)
+        {
+            for(int j =0; j < 2; j++)
+            {
+                v[i][j] += m.v[i][j];
+            }
+        }
+    }
+    void operator+(const Matrix& m){
+        add(m);
+    }
+private:
+    int v[2][2];
+};
+
 int main(int argc, char** argv) {
 
     return 0;
